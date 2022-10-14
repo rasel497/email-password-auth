16ff83000290813fb074b4a55d292a06494fc3d8
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-15 00:26:38 +0600

    Simple form, input, change, blur, submit, preventDefault

---
+++ src/__steps.js
@@ -0,0 +1,9 @@
+/*
+Auth Terminology:
+--------------------
+"Register" or "Sign up" [First time use a creater]
+1. "log in" "sign in" [Already u have a user account created]
+2.
+
+
+*/
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAmjfwlAGnib45DGp62K7hAjWwJu8vJ9NM",
+    authDomain: "email-password-auth-cd438.firebaseapp.com",
+    projectId: "email-password-auth-cd438",
+    storageBucket: "email-password-auth-cd438.appspot.com",
+    messagingSenderId: "997595319594",
+    appId: "1:997595319594:web:d9d2989275dfbfd550a253"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
