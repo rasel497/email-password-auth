0d9ddd03e001634315ef79ad5f15ddd28d200981
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-15 13:38:42 +0600

    create new user using email and password on firebase

--- src/componants/RegisterReactBootstrap.js
+++ src/componants/RegisterReactBootstrap.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from '../firebase/firebase.init';
 
+
+//01. start firebase setup
+const auth = getAuth(app)
 
 const RegisterReactBootstrap = () => {
     const handleRegisterOnSubmit = event => {
@@ -10,6 +15,15 @@ const RegisterReactBootstrap = () => {
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password);
+        //02. start firebase setup
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => {
+                console.error('error: ', error);
+            })
     }
 
     return (
@@ -18,12 +32,12 @@ const RegisterReactBootstrap = () => {
             <Form onSubmit={handleRegisterOnSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" name="email" placeholder="Enter email" />
+                    <Form.Control type="email" name="email" placeholder="Enter email" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="password" placeholder="Password" />
+                    <Form.Control type="password" name="password" placeholder="Password" required />
                 </Form.Group>
                 {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
